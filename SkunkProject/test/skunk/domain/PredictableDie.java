ce4c8e5ae2607eb33d97c40edb9ff242743d612b
Kai <sunx0229@umn.edu>
2020-10-02 20:28:08 -0500

    2nd passing PDie test...

--- SkunkProject/test/skunk/domain/PredictableDie.java
+++ SkunkProject/test/skunk/domain/PredictableDie.java
@@ -16,7 +16,8 @@ public class PredictableDie {
 	}
 
 	public void roll() {
-		this.lastRoll = this.theRolls[this.nextInt];		
+		this.lastRoll = this.theRolls[this.nextInt];
+		this.nextInt++;
 	}
 
 	public int getLastRoll() {
